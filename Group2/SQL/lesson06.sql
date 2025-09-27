343774560234a281e2970dca1c3c6d4e5e5d9d91
HusanAshirov <ashirovx6@gmail.com>
2025-09-27 12:43:41 +0500

    Create lesson06.sql

---
+++ Group2/SQL/lesson06.sql
@@ -0,0 +1,13 @@
+create table city_population (ID int, City  varchar(50),Population int, year int)
+
+insert into city_population values
+(1,'Chilonzor',2500,2012   ),
+(2,'Yakkasaroy',1500,2012  ),
+(3,'Mirobod',3000,2012     ),
+(4,'Yashnobod',1000,2012   ),
+(5,'Bektemir',2000,2012     ),
+(1,'Chilonzor',2800,2013   ),
+(2,'Yakkasaroy',1900,2013  ),
+(3,'Mirobod',2000,2013     ),
+(4,'Yashnobod',5000,2013   ),
+(5,'Bektemir',1500,2013     )
