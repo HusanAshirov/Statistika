3121150466d310bfc9175734f19204d8404365dc
HusanAshirov <ashirovx6@gmail.com>
2025-10-02 18:46:43 +0500

    Update lesson08.sql

--- Group1/lesson08.sql
+++ Group1/lesson08.sql
@@ -1,1 +1,5 @@
 write DDL statment to create Customers table with following columns like CustomerID int primary key, FirstName varchar(50), lastName varchar(50), Birthdate date insert 50 rows but customer names should be real-like names , customerid between 10001 and 10050 birtdate from 1970 to 2005
+
+
+
+write DDL statment to create FactSales table with following columns like SalesID int , ProductID int ,CustomerID int, SalesDate date, CustomerID int foreign key references Customer (CustomerID), employeeID int insert 200 rows but customer should within Customer table and only select 30 out of customers, SalesDate last 5 years, productID between 20001 and 20010, employeeID between 50001 and 50005
