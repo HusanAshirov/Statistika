d0bf8095b9dbc8c4f5268d2c8e7f34bec7be108f
HusanAshirov <ashirovx6@gmail.com>
2025-10-04 09:37:50 +0500

    Update lesson09.sql

--- Group1/lesson09.sql
+++ Group1/lesson09.sql
@@ -6,3 +6,13 @@ insert into Employee values
 	(3,'Fazliddin',1),
 	(4,'Bobomurod',3),
 	(5,'Hasan',4)
+
+
+----############################################################
+
+
+create table family (id int,name varchar(25),parentid int)
+insert into family values (1,'Grandfather',NULL),(2,'Father',1),
+(3,'Uncle',1),(4,'Me',2),(5,'My Son',4),(6,'Cousin',3),(7,'Cousin son',6)
+
+select * from family
