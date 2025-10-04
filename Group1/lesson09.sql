22e5b5c618ef6b7081548cf83deea82d14cb0de8
HusanAshirov <ashirovx6@gmail.com>
2025-10-04 09:21:30 +0500

    Create lesson09.sql

---
+++ Group1/lesson09.sql
@@ -0,0 +1,8 @@
+create table Employee (EmployeeID int, Name varchar(50),ManagerID int)
+
+insert into Employee values 
+	(1,'Shuxrat',null),
+	(2,'Dilmurod',1),
+	(3,'Fazliddin',1),
+	(4,'Bobomurod',3),
+	(5,'Hasan',4)
