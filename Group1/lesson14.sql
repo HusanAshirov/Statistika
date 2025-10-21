ac5c3d81c5aa77fd427d6a9c9725ba2e2e7f7e99
HusanAshirov <ashirovx6@gmail.com>
2025-10-21 19:46:17 +0500

    Update lesson14.sql

--- Group1/lesson14.sql
+++ Group1/lesson14.sql
@@ -50,3 +50,28 @@ INSERT INTO Customers VALUES
 (39, 'Mick', 'New York', 'Platinum'),
 (40, 'Noah', 'California', 'Gold');
 
+
+DROP TABLE IF EXISTS #Cart1;
+DROP TABLE IF EXISTS #Cart2;
+GO
+
+CREATE TABLE #Cart1
+(
+Item  VARCHAR(100) PRIMARY KEY
+);
+GO
+
+CREATE TABLE #Cart2
+(
+Item  VARCHAR(100) PRIMARY KEY
+);
+GO
+
+INSERT INTO #Cart1 (Item) VALUES
+('Sugar'),('Bread'),('Juice'),('Soda'),('Flour');
+GO
+
+INSERT INTO #Cart2 (Item) VALUES
+('Sugar'),('Bread'),('Butter'),('Cheese'),('Fruit');
+GO
+
